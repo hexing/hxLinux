bc7a82949e519057f0214046c8fca6b356ec961c
hexing <hexing.net@gmail.com>
2010-05-21 14:35:33 +0800

    initHOme.sh

--- script/utility/deliverIP.sh
+++ script/utility/deliverIP.sh
@@ -2,9 +2,9 @@
 
 ping -c 1 192.168.1.1 || exit
 
-MY_IP=`sh /usr/local/bin/getip.sh`
+MY_IP=`sh /usr/local/sbin/getip.sh`
 F_IP='/tmp/myIP.txt'
 if [ -r $F_IP ]; then
 	grep "$MY_IP" "$F_IP" >/dev/null && exit
 fi
-python /usr/local/bin/sendmail.py "$MY_IP" && echo $MY_IP > $F_IP
+python /usr/local/sbin/sendmail.py "$MY_IP" && echo $MY_IP > $F_IP
