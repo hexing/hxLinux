411109b37a2f7b5a9601e3734f726d8c95e1554a
hexing <hexiaoxing.net@gmail.com>
2011-01-08 23:31:15 +0800

    lilyterm sakura evilvte

---
+++ script/install_softwares/terminal_emulators.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+. ./Functions
+
+
+
+ARR_PACMAN=(
+lilyterm
+sakura
+)
+
+ARR_YAOURT=(
+evilvte
+)
+
+
+checkArgv $* || exit
+	
+optionType $@
+TYPE=$?
+if (( 1 == $TYPE )); then
+	for i in ${ARR_PACMAN[@]}; do
+		install $i
+	done
+
+	for i in ${ARR_YAOURT[@]}; do
+		install $i yaourt
+	done
+
+elif (( 2 == $TYPE )); then
+	for i in ${ARR_PACMAN[@]}; do
+		uninstall $i
+	done
+
+	for i in ${ARR_YAOURT[@]}; do
+		uninstall $i yaourt
+	done
+
+else
+	exit 99
+fi
