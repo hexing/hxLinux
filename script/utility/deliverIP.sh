d25c66d48c19d0129e60269bce5eef7e3fd2b836
hexing <hexiaoxing.net@gmail.com>
2011-01-24 23:24:40 +0800

    deliverip

--- script/utility/deliverIP.sh
+++ script/utility/deliverIP.sh
@@ -2,7 +2,7 @@
 
 ping -c 1 192.168.1.1 &>/dev/null || exit
 
-HX_BIN=$(dirname, "$0")
+HX_BIN=$(dirname "$0")
 MY_IP=`sh $HX_BIN/getip.sh` || exit
 F_IP='/tmp/myIP.txt'
 if [ -r $F_IP ]; then
--- script/utility/system_status.sh
+++ script/utility/system_status.sh
@@ -6,18 +6,29 @@ find /etc/ -type f -iname '*pacnew' -or -name '*~'
 find /opt/hxLinux -type f -name '*~'
 
 
+lsmod | grep '[i]pv6'
+
+
+NEED_PKG=(
+pdnsd #varnish
+)
+for i in ${NEED_PKG[@]}; do
+	pacman -Q $i 1>/dev/null
+done
+
+
 INGNORE_PKG=(
 xterm xorg-twm
 avahi
 )
-
 for i in ${INGNORE_PKG[@]}; do
 	pacman -Q $i 2>/dev/null
 done
 
+
 pacman -Qdt
 
 
-for i in $(ls ~/.recently-used.xbel*); do
+for i in $(find ~/ -name '.recently-used.xbel*'); do
 	echo $i
 done
