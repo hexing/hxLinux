e3a4f2e82535b3dce684998a2de5042d1f6c099d
hexing <hexiaoxing.net@gmail.com>
2011-01-24 17:42:04 +0800

    deliverIP

--- script/utility/backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-DIR_BACKUP=/opt/hxLinux
-
-LIST=('/etc/{hosts,resolv.conf,pacman.d/mirrorlist}')
-for i in ${LIST[@]}; do
-	eval ls $i
-done
--- script/utility/deliverIP.sh
+++ script/utility/deliverIP.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-ping -c 1 192.168.1.1 1>/dev/null || exit
+ping -c 1 192.168.1.1 &>/dev/null || exit
 
-HX_BIN=/usr/local/bin
-
+HX_BIN=$(dirname, "$0")
 MY_IP=`sh $HX_BIN/getip.sh` || exit
 F_IP='/tmp/myIP.txt'
 if [ -r $F_IP ]; then
